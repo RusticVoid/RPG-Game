dd4cafd9e813f4229b858df1de62f095028a0aa4
RusticVoid <99776353+RusticVoid@users.noreply.github.com>
2026-05-23 12:59:38 -0400

    Update utils.lua

--- src/utils.lua
+++ src/utils.lua
@@ -29,7 +29,7 @@ function loadClasses(dir)
 	OpSys = love.system.getOS()
 
 	if (OpSys == "Windows") then
-		files = io.popen([[dir ".\src\"]]..dir):lines()
+		files = io.popen([[dir /b ".\src\"]]..dir):lines()
 	end
 	if (OpSys == "Linux") then
 		files = io.popen([[ls ./src/]]..dir):lines()
